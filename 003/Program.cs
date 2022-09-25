e88cb44cb791a05bb08487a783cd5e5ea5f5b3a1
Artem <kypidon71@gmail.com>
2022-09-25 23:06:35 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("add N number");
+int num = int.Parse(Console.ReadLine());
+for(int Nik = 1; Nik <= num; Nik++ )
+    {
+        int result = (int)Math.Pow(Nik,3);
+        Console.WriteLine($"your number: {result}");
+    }
